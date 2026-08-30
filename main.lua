58b58960816e0c4ad5799a91472ee9477a8d0170
musija <musijalt1@gmail.com>
2026-08-30 15:37:24 -0700

    state as table for Reasons I guess

--- main.lua
+++ main.lua
@@ -7,36 +7,38 @@ local Button = require("ui/button")
 
 local w, h = love.graphics.getDimensions()
 local font = love.graphics.getFont()
-local state = 'menu'
+local state = {}
+--[[
+    state[""] = "menu" (in menus), "pause" (paused), or "gaming" (playing the game). 
+    state["menu"] = the screen of the menu. e.g. "main"
+]]--
 
 local test = 0
 
 function love.load() -- when game opens
-    state = 'gaming'
-    love.window.setTitle( "if you need to take a break from the stress of clicking the rectangle you can now PAUSE the game." )
-    b1 = Button(w/4,h/4,w/2,h/2,"text",function()
-    test = test+1
-    end,{1,0.7,0})
+    state[""] = "gaming"
+    state["menu"] = "main" 
+    love.window.setTitle( "okay now an actual 'game'" )
 end
 
 function love.update(dt) -- dt = time to update last frame (thus expected time for this frame)
-	if state == 'menu' then
-        updatemenu(dt)
-    elseif state == 'pause' then
-        updatepause(dt)
-    elseif state == 'gaming' then
-        b1:update()
-        updategaming(dt)
+    local s = state[""]
+	if s == "menu" then
+        updatemenu(dt, state)
+    elseif s == "pause" then
+        updatepause(dt,state)
+    elseif s == "gaming" then
+        updategaming(dt,state)
     else 
     -- damage control
     end
 end
 
 function love.draw(dt) -- rendering
-	b1:draw()
-    love.graphics.setColor({255,255,255})
+    local s = state[""]
+    love.graphics.setColor({1,0.6,0})
     love.graphics.print(test, w/4, 3*h/4)
-    if state == 'pause' then
+    if s == "pause" then
         love.graphics.setColor(0,0,0,0.2)
         love.graphics.rectangle("fill",0,0,w,h)
     end
@@ -65,14 +67,16 @@ local function isheld(x)
     end
 end
 
-function love.keypressed(k) --
-    if state == 'pause' then
+function love.keypressed(k)
+    local s = state[""]
+    if s == "pause" then
         -- keybinds when paused
-        if iskey(k,"pause") then state = 'gaming' end
-    elseif state == 'menu' then
+        if iskey(k,"pause") then state[""] = "gaming"
+        test = test + 1 end
+    elseif s == "menu" then
         -- keybinds when in menu
-    elseif state == 'gaming' then
+    elseif s == "gaming" then
         -- keybinds while in-game.
-        if iskey(k,"pause") then state = 'pause' end
+        if iskey(k,"pause") then state[""] = "pause" end
     end
 end
