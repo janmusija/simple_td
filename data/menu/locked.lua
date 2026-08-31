7f27bbaa689b447c71edb486d104a6f6a4e3af86
musija <musijalt1@gmail.com>
2026-08-31 00:26:54 -0700

    member funcs

--- data/menu/locked.lua
+++ data/menu/locked.lua
@@ -2,19 +2,20 @@
 
 local locked = {
     locks = {},
-    register_lock = function(menu_id,unlock_condition)
-        locked.locks[menu_id] = unlock_condition
-    end
 }
 
-local function LOCK_BY_LEVEL(menu_id,unlock_level,state)
+locked.register_lock = function(menu_id,unlock_condition)
+    locked.locks[menu_id] = unlock_condition
+end
+
+locked.LOCK_BY_LEVEL = function (menu_id,unlock_level,state)
     locked.register_lock(menu_id, function()
         return state.playerdata.completed_levels[unlock_level] == true
     end
     )
 end
 
-local function PERMALOCK(menu_id,unlock_level)
+locked.PERMALOCK = function(menu_id,unlock_level)
     locked.register_lock(menu_id, function() return false end)
 end
 
@@ -23,16 +24,16 @@ locked.runThisInMainToAccessRelevantParameters_ThereIsProbablyABetterWayToDoThis
     for i = 1,4 do
         for j = 1,5 do
             for k = 1,9 do
-                LOCK_BY_LEVEL("c" .. i .. "w" .. j .. "l" .. (k+1),"c" .. i .. "w" .. j .. "l" .. k,state)
+                locked.LOCK_BY_LEVEL("c" .. i .. "w" .. j .. "l" .. (k+1),"c" .. i .. "w" .. j .. "l" .. k,state)
             end
         end
         for j = 1,4 do
-            LOCK_BY_LEVEL("c" .. i .. "world" .. (j+1), "c" .. i .. "w" .. j .. "l" .. 10,state)
+            locked.LOCK_BY_LEVEL("c" .. i .. "world" .. (j+1), "c" .. i .. "w" .. j .. "l" .. 10,state)
         end
     end
-    LOCK_BY_LEVEL("campaign2", "c1w5l10",state)
-    LOCK_BY_LEVEL("campaign3", "c2w5l10",state)
-    LOCK_BY_LEVEL("campaign4", "c2w5l10",state) -- yes, 2.
+    locked.LOCK_BY_LEVEL("campaign2", "c1w5l10",state)
+    locked.LOCK_BY_LEVEL("campaign3", "c2w5l10",state)
+    locked.LOCK_BY_LEVEL("campaign4", "c2w5l10",state) -- yes, 2.
 end
 
 
