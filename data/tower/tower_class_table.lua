5a9812ec7a34668b41c96f445c9fbb650295c2b9
musija <musijalt1@gmail.com>
2026-08-31 17:21:15 -0700

    ????

--- data/tower/tower_class_table.lua
+++ data/tower/tower_class_table.lua
@@ -1,7 +1,36 @@
---local Mana_Orb = require("data/tower/towers/mana_orb")
---local Shooter = require("data/tower/towers/shooter")
+local Mana_Orb = require("data/tower/towers/mana_orb")
+local Shooter = require("data/tower/towers/shooter")
 
-return {
-    --shooter = Shooter,
-    --mana_orb = Mana_Orb
+local t_c_t = {}
+
+t_c_t.get = function(id)
+    assert(t_c_t.table[id] ~= nil, "Tower id" .. id .. "not seen in data/tower/towers/tower_class_table.lua!")
+    if t_c_t.table[id].__typ == nil then
+        return t_c_t.table[id]
+    elseif type(t_c_t.table[id]) == "table" then
+        return t_c_t.table[id].__typ
+    else
+        assert(false, "Invalid type" .. type(t_c_t.table[id]) .. "in table at" .. id)
+    end
+end
+
+t_c_t.mods = function(id)
+    assert(t_c_t.table[id] ~= nil, "Tower id" .. id .. "not seen in data/tower/tower/tower_class_table.lua!")
+    if t_c_t.table[id].__typ == nil then
+        local dummy = {}
+        return dummy
+    elseif type(t_c_t.table[id] == "table") then
+        return t_c_t.table[id]
+    else 
+        assert(false, "Invalid type" .. type(t_c_t.table[id]) .. "in table at" .. id)
+    end
+end
+
+
+
+t_c_t.table = {
+    shooter = Shooter,
+    mana_orb = Mana_Orb,
 }
+
+return t_c_t
